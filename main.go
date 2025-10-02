3a25b403567c6e0df158ec83cc33c48da3b0f1a4
Rustam Yusupov <i@rstm.me>
2025-10-02 18:20:56 +0200

    Add main package with basic HTTP server implementation

--- main.go
+++ main.go
@@ -6,6 +6,10 @@ import (
 )
 
 func main() {
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		w.Write([]byte("Welcome to Auth"))
+	})
+
 	log.Println("listening on :8080")
 	if err := http.ListenAndServe(":8080", nil); err != nil {
 		log.Fatal(err)
