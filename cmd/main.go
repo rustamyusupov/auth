9518e31ff8943f68b65e99acab1b463454c88e44
Rustam Yusupov <i@rstm.me>
2025-10-02 17:48:56 +0200

    Initialize main package with basic HTTP server setup

--- cmd/main.go
+++ cmd/main.go
@@ -1,7 +1,13 @@
 package main
 
-import "fmt"
+import (
+	"log"
+	"net/http"
+)
 
 func main() {
-	fmt.Println("Hello, World!")
+	log.Println("listening on :8080")
+	if err := http.ListenAndServe(":8080", nil); err != nil {
+		log.Fatal(err)
+	}
 }
